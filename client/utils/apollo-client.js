c53312e8eb9379c34fc05bc519d6d3a9732a32d1
Alesha Tikhonov <zukko961@gmail.com>
2023-03-18 11:28:46 +0300

    add aoi url

--- client/utils/apollo-client.js
+++ client/utils/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:3001/graphql',
+    uri: 'https://sterilno-back.herokuapp.com/graphql',
     cache: new InMemoryCache(),
   });
 
